f21cb158a58514f2f2e978e8917e66fa8684ba29
Evandro <eignacio403@gmail.com>
2019-03-14 20:52:35 -0300

    as tabelas do banco de dados foram criadas por migrations

---
+++ database/migrations/2019_03_14_232031_create_atividades_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAtividadesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('atividades', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome');
+            $table->integer('a_familia_id')->unsigned();
+            $table->foreign('a_familia_id')->references('id')->on('familia');
+            $table->integer('a_user_id')->nullable()->unsigned();
+            $table->foreign('a_user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('atividades');
+    }
+}
---
+++ database/migrations/2019_03_14_232203_create_membros_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMembrosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('membros', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('m_user_id')->unsigned();
+            $table->foreign('m_user_id')->references('id')->on('users');
+            $table->integer('m_familia_id')->unsigned();
+            $table->foreign('m_familia_id')->references('id')->on('familia');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('membros');
+    }
+}
