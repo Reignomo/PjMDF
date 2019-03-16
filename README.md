cc3105fc25891928a5c4b2f931783532349faccf
Evandro <eignacio403@gmail.com>
2019-03-16 10:51:53 -0300

    foi adicionada a tabela/model/controller da classe medalha

--- README.md
+++ README.md
@@ -9,6 +9,6 @@ The MDF is a responsive web application for family activities control.
 - 1  ```Composer.phar install```
 - 2  copy ev. and change the database credentials https://github.com/laravel/laravel/blob/master/.env.example 
 - 3 ``` git clone https://github.com/lucascudo/laravel-5.7-pt-BR-localization.git ```
-- 4 ```php artisan migration ```
+- 4 ```php artisan migrate ```
 
 
---
+++ database/migrations/2019_03_16_133559_create_medalhas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMedalhasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('medalhas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('pontuacao'); #o usúario ganha medalhas de acordo com a quantidade de pontuações - Pontuação 0-50: Aspirante - Pontuação 50-100: Guerreiro - Pontuação 100-150 Perito - Pontuação: 150-200 Mestre
+            $table->integer('m_user_id')->unsigned();
+            $table->foreign('m_user_id')->references('user');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('medalhas');
+    }
+}
