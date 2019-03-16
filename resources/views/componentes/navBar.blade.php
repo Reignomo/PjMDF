9254e87b84698b51a12b47d74f7d9116516d8f72
Evandro <eignacio403@gmail.com>
2019-03-16 11:29:05 -0300

    layout da pagina home foi alterado

--- resources/views/componentes/navBar.blade.php
+++ resources/views/componentes/navBar.blade.php
@@ -10,18 +10,11 @@
       <li @if($current =="home") class="nav-item active" @else class="nav-item" @endif>
         <a class="nav-link" href="/">Home</a>
       </li>
-      <li @if($current =="clientes") class="nav-item active dropdown" @else class="nav-item dropdown" @endif>
-        <a class="nav-link dropdown-toggle" id="linkClientes" role="button" data-toggle="dropdown" href="#">Clientes</a>
+      <li @if($current =="Familias") class="nav-item active dropdown" @else class="nav-item dropdown" @endif>
+        <a class="nav-link dropdown-toggle" id="linkClientes" role="button" data-toggle="dropdown" href="#">Familia</a>
         <div class="dropdown-menu" aria-labelledby="linkClientes">
-          <a class="dropdown-item" href="/clientes">Lista de clientes</a>
-          <a class="dropdown-item" href="/cliente/novo">Cadastro</a>
-        </div>
-      </li>
-      <li @if($current =="departamentos") class="nav-item active dropdown" @else class="nav-item dropdown" @endif>
-      <a class="nav-link dropdown-toggle" id="linkClientes" role="button" data-toggle="dropdown" href="#">Departamentos</a>
-        <div class="dropdown-menu" aria-labelledby="linkClientes">
-          <a class="dropdown-item" href="/departamentos">Lista de departamentos</a>
-          <a class="dropdown-item" href="/departamento/novo">Cadastro</a>
+          <a class="dropdown-item" href="/clientes">Adicionar grupo familiar</a>
+          <a class="dropdown-item" href="/cliente/novo">Gerenciar atividades</a>
         </div>
       </li>
       <li @if($current =="about") class="nav-item active" @else class="nav-item" @endif>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,13 +6,17 @@
         border-radius:5%;
     }
 </style>
-    <div class="bs-callout bs-callout-primary bg-white" style="margin-bottom:60px;">
+    <div class="bd-example">
+        <a class="btn btn-primary" href="#!">Familia 1</a> <!-- tornar ativo o botão da familia que está aparecendo na tela -->
+        <a class="btn btn-outline-primary" href="#!">Familia 2</a>
+    </div>
+    <div class="bs-callout bs-callout-primary bg-white" style="margin-bottom:60px;margin-top:5px;">
         <div class="card-body">
             <div class="row centered">
                 <div class="col-xs-12 col-md-3 centered">
                     <img alt="Responsive image" src="/img/familiaExemple.jpg" class="img-card-family img-thumbnail">
                 </div>
-                <div class="col-xs-12 col-md-5 centered text-center">
+                <div class="col-xs-12 col-md-5 centered">
                     <h3 class="card-title text-primary">Nome da familia</h3>
                     <div class="text-secondary">
                         <p><b>Quantidade de membros:</b> 25</p>
@@ -30,7 +34,17 @@
         </div>
     </div>
 
-    <div class="card border">
+    <div class="bd-example">
+        <a class="btn btn-primary" href="!#">Segunda</a> <!-- tornar ativo o botão do dia da semana atual -->
+        <a class="btn btn-outline-primary" href="#!">Terça</a>
+        <a class="btn btn-outline-primary" href="#!">Quarta</a>
+        <a class="btn btn-outline-primary" href="#!">Quinta</a>
+        <a class="btn btn-outline-primary" href="#!">Sexta</a>
+        <a class="btn btn-outline-primary" href="#!">Sábado</a>
+        <a class="btn btn-outline-primary" href="#!">Domingo</a>
+    </div>
+    <div class="card border" style="margin-top:5px; margin-bottom:5px;">
         ss
     </div>
+    <a class="btn btn-primary float-right" href="!#"><i class="fas fa-plus"></i></a>
 @endSection
